b35d77ccbbf42409cd0367881750655c51b7dfa1
Gaby Markley <gabrielle.markley.505@my.csun.edu.com>
2023-08-09 13:19:05 -0700

    Up to tab 2 num 11

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,22 @@
+export default function SignUpForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    
+    return {
+        async function handleSubmit(e) {
+            e.preventDefault();
+            console.log("Thank you for your submission");
+        }
+        <h2> Sign Up </h2>;
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username: <input value={username} onChange={(e) =>setUsername(e.target.value)}/>
+            </label>
+            <label>
+                Password: <input value={password} onChange={(e) =>setPassword(e.target.value)}/>
+            </label>
+                <button>Submit</button>
+        </form>
+    
+}
