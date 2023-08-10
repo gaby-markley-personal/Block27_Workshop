510d5764b4323a474c827830f93bdce02bd2ca19
Gaby Markley <gabrielle.markley.505@my.csun.edu.com>
2023-08-10 07:52:46 -0700

    added api to async function

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -3,14 +3,21 @@ export default function SignUpForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
-
+// need to add an await below but i'm nto exactly sure where 
   async function handleSubmit(e) {
     e.preventDefault();
     console.log("Thank you for your submission");
+    try { fetch('https://fsa-jwt-practice.herokuapp.com/signup')
+    } catch (error) {
+        setError(error.message);
+    }
+    
   }
   return (
+
     <div>
       <h2> Sign Up </h2>
+      {error && <p> {error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
           Username:{" "}
@@ -30,4 +37,4 @@ export default function SignUpForm() {
       </form>
     </div>
   );
-}
+  }
