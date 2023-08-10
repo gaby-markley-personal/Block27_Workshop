1efb6af27c96ef46952a1bd3fd7f39c32bcdf069
Gaby Markley <gabrielle.markley.505@my.csun.edu.com>
2023-08-10 07:29:29 -0700

    sign up commit

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,22 +1,33 @@
+import { useState } from "react";
 export default function SignUpForm() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
-    
-    return {
-        async function handleSubmit(e) {
-            e.preventDefault();
-            console.log("Thank you for your submission");
-        }
-        <h2> Sign Up </h2>;
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username: <input value={username} onChange={(e) =>setUsername(e.target.value)}/>
-            </label>
-            <label>
-                Password: <input value={password} onChange={(e) =>setPassword(e.target.value)}/>
-            </label>
-                <button>Submit</button>
-        </form>
-    
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Thank you for your submission");
+  }
+  return (
+    <div>
+      <h2> Sign Up </h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
